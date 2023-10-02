a7d3f93e3c1a9ee08c0b4f8b9ab431c40907e14b
Jayachandran-Venkatesh <129407089+Jayachandran-Venkatesh@users.noreply.github.com>
2023-10-02 18:06:39 +0000

    1st commit

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-with open(r"_chat.txt", 'r') as fp:
+# person's who interact with admin.
+
+with open("_chat.txt", 'r', encoding='utf-8') as fp:
     text = fp.readlines()
-    lines = len(text)
-    pollcount =0
-    for eachline in text:
-        if 'POLL' in eachline:
-            pollcount=pollcount+1
-    print('Total Number of lines:', lines)
-    print('Total polls: '+str(pollcount))
     
+# Create an empty set to store unique user names
+user_names = set()
+
+# Iterate through each line in the text
+for line in text:
+    # Check if the line contains 'sir'
+    if 'sir' in line.lower():
+        # Extract the user name from the line
+        parts = line.split(']')  # Split the line by ']'
+        if len(parts) >= 2:
+            user_name = parts[1].strip().split(':')[0]
+            user_names.add(user_name)
+user_names.remove('ThiyagaB')
+# Print the unique user names who mentioned 'sir'
+for name in user_names:
+    print(name)
